438a1ac00eab3ed5970e410bf05f01881e2959ca
이장훈 <chaossky@naver.com>
2017-04-20 23:06:30 +0900

    추가

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,20 @@
+﻿//
+//  main.cpp
+//  Pointer_exercise
+//
+//  Created by 이장훈 on 2016. 12. 16..
+//  Copyright © 2016년 이장훈. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	int a = 10;
+	int *p = &a;
+
+
+	std::cout << "포인터 값은 :" << *p << std::endl << "주소값은 :" << p << "   " << std::endl;
+	std::cout << &a << std::endl;
+	return 0;
+}
---
+++ test2/test2/test2.cpp
@@ -0,0 +1,14 @@
+﻿
+#include "stdafx.h"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	int a = 10;
+	int *p = &a;
+
+
+	std::cout << "포인터 값은 :" << *p << std::endl << "주소값은 :" << p << "   " << std::endl;
+	std::cout << &a << std::endl;
+	return 0;
+}
